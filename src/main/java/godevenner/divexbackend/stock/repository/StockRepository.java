a4037d5dc5b0052d03fb81b6f8aeaef2597c7f87
Chris <chye0001@stud.kea.dk>
2024-11-25 21:05:10 +0100

    added costume JPQL method to StockRepository.java

--- src/main/java/godevenner/divexbackend/stock/repository/StockRepository.java
+++ src/main/java/godevenner/divexbackend/stock/repository/StockRepository.java
@@ -3,5 +3,9 @@ package godevenner.divexbackend.stock.repository;
 import godevenner.divexbackend.stock.model.Stock;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface StockRepository extends JpaRepository<Stock, Integer> {
+
+    Optional<Stock> findByTicker(String code);
 }
