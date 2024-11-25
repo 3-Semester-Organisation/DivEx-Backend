7f7a0fb9ff4d9a2acdcd97bd80129803fdb58cb3
Chris <chye0001@stud.kea.dk>
2024-11-25 21:15:23 +0100

    added methods into StockService.java and impl, for pagination

--- src/main/java/godevenner/divexbackend/stock/service/StockServiceImpl.java
+++ src/main/java/godevenner/divexbackend/stock/service/StockServiceImpl.java
@@ -6,6 +6,8 @@ import godevenner.divexbackend.stock.mapper.StockResponseMapper;
 import godevenner.divexbackend.stock.model.Stock;
 import godevenner.divexbackend.stock.repository.StockRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,6 +16,14 @@ public class StockServiceImpl implements StockService {
 
     private final StockRepository stockRepository;
     private final StockResponseMapper stockResponseMapper;
+
+
+
+    @Override
+    public Page<StockResponse> getAllStocks(Pageable pageable) {
+        return stockRepository.findAll(pageable).map(stockResponseMapper::toStockResponse);
+    }
+
 
     @Override
     public StockResponse getStock(String ticker) {
