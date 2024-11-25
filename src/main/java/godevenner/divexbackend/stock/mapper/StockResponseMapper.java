8d03907e4f1c0e1fdde3eaf046fb5315157f1d35
Chris <chye0001@stud.kea.dk>
2024-11-25 21:03:50 +0100

    created StockResponseMapper.java

---
+++ src/main/java/godevenner/divexbackend/stock/mapper/StockResponseMapper.java
@@ -0,0 +1,59 @@
+package godevenner.divexbackend.stock.mapper;
+
+import godevenner.divexbackend.stock.dto.HistoricalDividendsResponse;
+import godevenner.divexbackend.stock.dto.HistoricalPricingResponse;
+import godevenner.divexbackend.stock.dto.StockResponse;
+import godevenner.divexbackend.stock.model.Dividend;
+import godevenner.divexbackend.stock.model.Stock;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class StockResponseMapper {
+
+    public StockResponse toStockResponse(Stock stock) {
+        Dividend dividend = stock.getDividend();
+
+        return new StockResponse(
+                stock.getTicker(),
+                stock.getName(),
+                stock.getCountry(),
+                stock.getExchange(),
+                stock.getCurrency(),
+                stock.getIndustry(),
+                stock.getSector(),
+
+                toHistoricalPricingResponse(stock),
+
+                dividend.getDividendRate(),
+                dividend.getDividendYield(),
+                dividend.getDividendRatio(),
+                dividend.getFiveYearAvgDividendYield(),
+                dividend.getExDividendDate(),
+
+                toHistoricalDividendsResponse(stock)
+        );
+
+
+    }
+
+    private List<HistoricalPricingResponse> toHistoricalPricingResponse(Stock stock) {
+        return stock.getHistoricalPricings().stream()
+                .map(historicalPricing -> new HistoricalPricingResponse(
+                        historicalPricing.getOpeningPrice(),
+                        historicalPricing.getOpeningDate(),
+                        historicalPricing.getPreviousDailyClosingPrice(),
+                        historicalPricing.getClosingDate()
+                )).toList();
+    }
+
+    private List<HistoricalDividendsResponse> toHistoricalDividendsResponse(Stock stock) {
+        return stock.getHistoricalDividends().stream()
+                .map(historicalDividend -> new HistoricalDividendsResponse(
+                        historicalDividend.getDividendRate(),
+                        historicalDividend.getExDividendDate()
+                )).toList();
+    }
+}
