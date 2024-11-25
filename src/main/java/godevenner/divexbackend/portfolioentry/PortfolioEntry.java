0a6377ce98a23be6b7ab44ab06db944126bc99af
Chris <chye0001@stud.kea.dk>
2024-11-25 13:56:13 +0100

    created PortfolioEntry.java and created relationship between Stock and Portfolio

---
+++ src/main/java/godevenner/divexbackend/portfolioentry/PortfolioEntry.java
@@ -0,0 +1,30 @@
+package godevenner.divexbackend.portfolioentry;
+
+import godevenner.divexbackend.portfolio.Portfolio;
+import godevenner.divexbackend.stock.model.Stock;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PortfolioEntry {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @ManyToOne
+    private Portfolio portfolio;
+
+    @ManyToOne
+    private Stock stock;
+}
