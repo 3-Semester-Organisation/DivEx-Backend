a10f390b728ceb8c0a803f8f4d9528003bfcda94
Chris <chye0001@stud.kea.dk>
2024-11-25 15:42:01 +0100

    removed fields related to Stock

--- src/main/java/godevenner/divexbackend/stock/model/HistoricalDividend.java
+++ src/main/java/godevenner/divexbackend/stock/model/HistoricalDividend.java
@@ -1,9 +1,6 @@
 package godevenner.divexbackend.stock.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDate;
@@ -21,7 +18,4 @@ public class HistoricalDividend {
 
     private double dividendRate;
     private LocalDate exDividendDate;
-
-    @ManyToOne
-    private Stock stock;
 }
