a1d8bd3746067849c6f6908d61c042491df66f0e
Chris <chye0001@stud.kea.dk>
2024-11-25 14:15:55 +0100

    created HistoricalPricing and an unidirectional relationship to Stock

---
+++ src/main/java/godevenner/divexbackend/stock/model/historicalpricing/HistoricalPricing.java
@@ -0,0 +1,31 @@
+package godevenner.divexbackend.stock.model.historicalpricing;
+
+import godevenner.divexbackend.stock.model.Stock;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class HistoricalPricing {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    private double previousDailyClosingPrice;
+    private LocalDate closingDate;
+
+    @ManyToOne
+    private Stock stock;
+}
