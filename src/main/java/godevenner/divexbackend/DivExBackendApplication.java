51e9d4e290e2324fa66c24e6d87061633bb1fe97
Christoffer <chri26eq@stud.kea.dk>
2024-11-25 11:27:24 +0100

    init

---
+++ src/main/java/godevenner/divexbackend/DivExBackendApplication.java
@@ -0,0 +1,13 @@
+package godevenner.divexbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DivExBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DivExBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/godevenner/divexbackend/DivExBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package godevenner.divexbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DivExBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
