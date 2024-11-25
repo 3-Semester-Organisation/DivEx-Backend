4d1c1d782b3cac3d37b5da5ffa8ba1d395e992df
Lars <144362461+laha0006@users.noreply.github.com>
2024-11-25 15:31:50 +0100

    changed strategy of generated value to identity

--- src/main/java/godevenner/divexbackend/stock/model/HistoricalDividend.java
+++ src/main/java/godevenner/divexbackend/stock/model/HistoricalDividend.java
@@ -1,9 +1,6 @@
 package godevenner.divexbackend.stock.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDate;
@@ -16,7 +13,7 @@ import java.time.LocalDate;
 public class HistoricalDividend {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     private double dividendRate;
