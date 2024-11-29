4c580fa14d59bcc721fc6b7d04002ac2c66d7591
madsfjeldberg <mafj0001@stud.kea.dk>
2024-11-29 17:41:08 +0100

    temp security config

--- src/main/java/godevenner/divexbackend/config/SecurityConfig.java
+++ src/main/java/godevenner/divexbackend/config/SecurityConfig.java
@@ -37,6 +37,8 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
 
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/v1/**").permitAll()
+                        //.anyRequest().authenticated())
                         .anyRequest().permitAll())
 
                 .sessionManagement(session -> session
@@ -52,7 +54,9 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         // Set up CORS configuration
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:*"));
+        configuration.setAllowedOrigins(List.of(
+                "http://localhost:5173"
+        ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
