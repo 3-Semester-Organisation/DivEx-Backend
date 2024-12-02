ca1f5006fa680e279dc72f525db4d7d09ddd4ef9
Lars <144362461+laha0006@users.noreply.github.com>
2024-12-02 15:40:51 +0100

    added TrackLogRepository

---
+++ src/main/java/godevenner/divexbackend/tracker/TrackLogRepository.java
@@ -0,0 +1,6 @@
+package godevenner.divexbackend.tracker;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TrackLogRepository extends JpaRepository<TrackLog, Long> {
+}
