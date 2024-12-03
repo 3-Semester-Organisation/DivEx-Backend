efaff639f2cfe4b92392cd3ebe70c7de721f5b8e
Chris <chye0001@stud.kea.dk>
2024-12-03 16:08:18 +0100

    recreated test for PortfolioEntryServiceImplTest

--- src/test/java/godevenner/divexbackend/portfolio/service/PortfolioEntryServiceImplTest.java
+++ src/test/java/godevenner/divexbackend/portfolio/service/PortfolioEntryServiceImplTest.java
@@ -1,13 +1,8 @@
 package godevenner.divexbackend.portfolio.service;
 
-import godevenner.divexbackend.portfolio.model.Portfolio;
 import godevenner.divexbackend.portfolio.model.PortfolioEntry;
 import godevenner.divexbackend.portfolio.repository.PortfolioEntryRepository;
-import godevenner.divexbackend.portfolio.repository.PortfolioRepository;
 import godevenner.divexbackend.stock.model.*;
-import godevenner.divexbackend.subscription.model.Subscription;
-import godevenner.divexbackend.subscription.model.SubscriptionType;
-import godevenner.divexbackend.user.model.User;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -15,8 +10,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
-import java.time.LocalDateTime;
-import java.time.Month;
 import java.util.List;
 
 import static org.mockito.Mockito.*;
@@ -24,8 +17,6 @@ import static org.mockito.Mockito.*;
 class PortfolioEntryServiceImplTest {
     @Mock
     PortfolioEntryRepository portfolioEntryRepository;
-    @Mock
-    PortfolioRepository portfolioRepository;
     @InjectMocks
     PortfolioEntryServiceImpl portfolioEntryServiceImpl;
 
@@ -35,45 +26,12 @@ class PortfolioEntryServiceImplTest {
     }
 
     @Test
-    void testGetPortfolioEntries() {
-        when(portfolioEntryRepository.findByPortfolioId(anyLong())).thenReturn(List.of(new PortfolioEntry(1L,
-                new Portfolio(0L, "portfolioName",
-                        new User(1L, "username", "email", "password", "firstName", "lastName", "phone", "address", "city",
-                                new Subscription(1L, true, SubscriptionType.PREMIUM, LocalDateTime.of(2024, Month.NOVEMBER, 30, 23, 17, 15), LocalDateTime.of(2024, Month.NOVEMBER, 30, 23, 17, 15)))),
-                new Stock(0L, "ticker", "portfolioName", "country", "exchange", Currency.DKK, "industry", "sector",
-                        new Dividend(0L, 0d, 0d, 0d, 0d, 0L),
-                        List.of(new HistoricalDividend(0L, 0d, 0L)),
-                        List.of(new HistoricalPricing(0L, 0d, 0L, 0d, 0L))))));
-
-        List<PortfolioEntry> result = portfolioEntryServiceImpl.getPortfolioEntries(0L);
-        Assertions.assertEquals(List.of(new PortfolioEntry(1L,
-                new Portfolio(0L, "portfolioName", new User(1L, "username", "email", "password", "firstName", "lastName", "phone", "address", "city",
-                        new Subscription(1L, true, SubscriptionType.PREMIUM, LocalDateTime.of(2024, Month.NOVEMBER, 30, 23, 17, 15), LocalDateTime.of(2024, Month.NOVEMBER, 30, 23, 17, 15)))),
-                new Stock(0L, "ticker", "portfolioName", "country", "exchange", Currency.DKK, "industry", "sector",
-                        new Dividend(0L, 0d, 0d, 0d, 0d, 0L),
-                        List.of(new HistoricalDividend(0L, 0d, 0L)),
-                        List.of(new HistoricalPricing(0L, 0d, 0L, 0d, 0L))))), result);
-    }
-
-    @Test
     void testCreatePortfolioEntry() {
-        PortfolioEntry mockPortfolioEntry = new PortfolioEntry(1L,
-                new Portfolio(0L, "portfolioName",
-                        new User(1L, "username", "email", "password", "firstName", "lastName", "phone", "address", "city",
-                                new Subscription(1L, true, SubscriptionType.PREMIUM, LocalDateTime.of(2024, Month.NOVEMBER, 30, 23, 17, 15), LocalDateTime.of(2024, Month.NOVEMBER, 30, 23, 17, 15))
-                        )
-                ),
-                new Stock(0L, "ticker", "portfolioName", "country", "exchange", Currency.DKK, "industry", "sector",
-                        new Dividend(0L, 0d, 0d, 0d, 0d, 0L),
-                        List.of(new HistoricalDividend(0L, 0d, 0L)),
-                        List.of(new HistoricalPricing(0L, 0d, 0L, 0d, 0L))
-                )
-        );
+        when(portfolioEntryRepository.save(any(PortfolioEntry.class))).thenReturn(new PortfolioEntry(Long.valueOf(1), 0, new Stock(0L, "ticker", "name", "country", "exchange", Currency.DKK, "industry", "sector", new Dividend(0L, 0d, 0d, 0d, 0d, 0L), List.of(new HistoricalDividend(0L, 0d, 0L)), List.of(new HistoricalPricing(0L, 0d, 0L, 0d, 0L)))));
 
-        when(portfolioEntryRepository.save(any(PortfolioEntry.class))).thenReturn(mockPortfolioEntry);
-
-        PortfolioEntry result = portfolioEntryServiceImpl.createPortfolioEntry(mockPortfolioEntry);
-        Assertions.assertEquals(mockPortfolioEntry, result);
+        PortfolioEntry result = portfolioEntryServiceImpl.createPortfolioEntry(new PortfolioEntry(Long.valueOf(1), 0, new Stock(0L, "ticker", "name", "country", "exchange", Currency.DKK, "industry", "sector", new Dividend(0L, 0d, 0d, 0d, 0d, 0L), List.of(new HistoricalDividend(0L, 0d, 0L)), List.of(new HistoricalPricing(0L, 0d, 0L, 0d, 0L)))));
+        Assertions.assertEquals(new PortfolioEntry(Long.valueOf(1), 0, new Stock(0L, "ticker", "name", "country", "exchange", Currency.DKK, "industry", "sector", new Dividend(0L, 0d, 0d, 0d, 0d, 0L), List.of(new HistoricalDividend(0L, 0d, 0L)), List.of(new HistoricalPricing(0L, 0d, 0L, 0d, 0L)))), result);
     }
 }
 
+//Generated with love by TestMe :) Please raise issues & feature requests at: https://weirddev.com/forum#!/testme
