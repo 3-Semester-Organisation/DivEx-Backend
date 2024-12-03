95cf1c7cfbda249c90d28fc183c3e7da432b1301
Dosei-desu <krispauly@hotmail.com>
2024-12-03 17:16:17 +0100

    altered comparator to put highest visit count at the top rather than the bottom
    TODO: figure out why sorted by period doesn't work

--- src/main/java/godevenner/divexbackend/stock/PopularityComparator.java
+++ src/main/java/godevenner/divexbackend/stock/PopularityComparator.java
@@ -7,6 +7,6 @@ import java.util.Comparator;
 public class PopularityComparator implements Comparator<StockPopularity> {
     @Override
     public int compare(StockPopularity o1, StockPopularity o2) {
-        return Integer.compare(o1.visits(), o2.visits());
+        return Integer.compare(o2.visits(), o1.visits());
     }
 }
